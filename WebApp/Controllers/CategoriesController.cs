5975c2fd01eef2c34c2a4c36acd6af7b02de0ea3
Anny Gutierrez <annyggw@gmail.com>
2024-08-27 21:21:04 -0600

    Add project files.

---
+++ WebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }        
+        public IActionResult Edit(int? id)
+        {
+            //Instance of the Model
+            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+
+            return View(category);
+        }
+    }
+}
